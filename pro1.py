52dc295e65383f6f842e68fbdf733c90e4a81745
jayakumar98 <34593322+jayakumar98@users.noreply.github.com>
2017-12-16 11:55:52 +0530

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,13 @@
+a=int(input("Please enter the integer to be check: "))
+
+if(a>0):
+  
+      print("It is an positive integer")
+
+elif(a<0):
+    
+      print("It is an negative integer")
+
+else:
+   
+      print("It is zero")
