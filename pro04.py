43b1657c19d822bc05e6b8b357dffbae0b5f8ab0
jayakumar98 <34593322+jayakumar98@users.noreply.github.com>
2017-12-17 15:14:49 +0530

    Create pro04.py

---
+++ pro04.py
@@ -0,0 +1,5 @@
+x=raw_input('Enter a character: ')
+if((x>='a' and x<='z') or (x>='A' and x<='Z')):
+	print ('The given character is an alphabet')
+else:
+	print ('The given character is not an alphabet')
