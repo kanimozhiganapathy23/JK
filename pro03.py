cc0c8a3f1560d306de71c39ef0cd0ee3525050ca
jayakumar98 <34593322+jayakumar98@users.noreply.github.com>
2017-12-17 15:06:50 +0530

    Rename pro3.py to pro03.py

---
+++ pro03.py
@@ -0,0 +1,6 @@
+x=raw_input('Enter a character: ')
+y=['a','e','i','o','u','A','E','I','O','U']
+if x in y:
+	print('The given character is a vowel')
+else:
+	print('The given character is a consonant')
