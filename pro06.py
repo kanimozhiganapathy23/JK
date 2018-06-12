82e51cc31ade0fc20abbdd3e7a1860a1319d6f4a
kanimozhiganapathy23 <34593364+kanimozhiganapathy23@users.noreply.github.com>
2018-06-12 11:49:11 +0530

    Update pro06.py

--- pro06.py
+++ pro06.py
@@ -1,5 +1,10 @@
-x=int(input('Enter the year: \n'))
-if(x%4==0):
+x=int(input())
+if(x%100==0):
+	if(x%400==0):
+		print('The given year is leap year')
+	else:
+		print('The given year is not leap year')
+elif(x%4==0):
 	print('The given year is leap year')
 else:
 	print('The given year is not a leap year')
